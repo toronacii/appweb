42aecf85e7009dcc62f49dba8cf5535983cdaaa0
José Toro <toronacii@gmail.com>
2014-08-08 14:02:46 -0430

    Agregando archivo constants.php

---
+++ application/config/constants.php
@@ -0,0 +1,46 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/*
+|--------------------------------------------------------------------------
+| File and Directory Modes
+|--------------------------------------------------------------------------
+|
+| These prefs are used when checking and setting modes when working
+| with the file system.  The defaults are fine on servers with proper
+| security, but you may wish (or even need) to change the values in
+| certain environments (Apache running a separate process for each
+| user, PHP under CGI with Apache suEXEC, etc.).  Octal values should
+| always be used to set the mode correctly.
+|
+*/
+define('FILE_READ_MODE', 0644);
+define('FILE_WRITE_MODE', 0666);
+define('DIR_READ_MODE', 0755);
+define('DIR_WRITE_MODE', 0777);
+define('UT', 107);
+
+/*
+|--------------------------------------------------------------------------
+| File Stream Modes
+|--------------------------------------------------------------------------
+|
+| These modes are used when working with fopen()/popen()
+|
+*/
+
+define('FOPEN_READ',							'rb');
+define('FOPEN_READ_WRITE',						'r+b');
+define('FOPEN_WRITE_CREATE_DESTRUCTIVE',		'wb'); // truncates existing file data, use with care
+define('FOPEN_READ_WRITE_CREATE_DESTRUCTIVE',	'w+b'); // truncates existing file data, use with care
+define('FOPEN_WRITE_CREATE',					'ab');
+define('FOPEN_READ_WRITE_CREATE',				'a+b');
+define('FOPEN_WRITE_CREATE_STRICT',				'xb');
+define('FOPEN_READ_WRITE_CREATE_STRICT',		'x+b');
+define('TITLE_APP',		'Alcald&iacute;a WEB');
+define('API_DIR',	'http://localhost/appweb_server/public/api/v1');
+define('DIR_JSP', 'http://200.75.140.58:443/OnlinePayment/enviarPagoCredicard.do');
+/*RECAPTCHA*/
+define('RECAPTCHA_API_SERVER', 'http://www.google.com/recaptcha/api');
+define('RECAPTCHA_API_SECURE_SERVER', 'https://www.google.com/recaptcha/api');
+define('RECAPTCHA_VERIFY_SERVER', 'www.google.com');
+/* End of file constants.php */
+/* Location: ./application/config/constants.php */
