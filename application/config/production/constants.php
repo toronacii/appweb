cd0460a714f5c18cd490a0349a730d8024864073
= <=>
2015-01-04 20:56:14 -0430

    Production config

--- application/config/production/constants.php
+++ application/config/production/constants.php
@@ -36,14 +36,10 @@ define('FOPEN_READ_WRITE_CREATE'             , 	'a+b');
 define('FOPEN_WRITE_CREATE_STRICT'           , 	'xb');
 define('FOPEN_READ_WRITE_CREATE_STRICT'      , 	'x+b');
 define('TITLE_APP'                           , 	'Alcaldía WEB');
-define('API_DIR'                            , 	'http://localhost/appweb_server/public/api/v1');
+define('API_DIR'                            ,     'http://172.16.1.2/api.appweb/api/v1');
 
-define('ONLINE_PAYMENT', "http://localhost/online_payment/api/");
-define('BANESCO_ONLINE', "https://200.71.151.226:8443/payment/action/paymentgatewayuniversal-data?control=");
+define('ONLINE_PAYMENT', "http://172.16.1.8/online_payment/api/");
+define('BANESCO_ONLINE', "https://payment.megasoft.com.ve/payment/action/paymentgatewayuniversal-data?control=");
 
-/*RECAPTCHA*/
-define('RECAPTCHA_API_SERVER'                , 'http://www.google.com/recaptcha/api');
-define('RECAPTCHA_API_SECURE_SERVER'         , 'https://www.google.com/recaptcha/api');
-define('RECAPTCHA_VERIFY_SERVER'             , 'www.google.com');
 /* End of file constants.php */
 /* Location: ./application/config/constants.php */
