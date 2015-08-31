d7d2d09023cc2d3b33890d894f8b0c31e7b6e588
José Toro <toronacii@gmail.com>
2015-08-31 15:44:43 -0430

    add nuevas credenciales

--- application/config/production/megasoft.php
+++ application/config/production/megasoft.php
@@ -5,7 +5,7 @@ $url_pro = "https://payment.megasoft.com.ve/payment/action/paymentgatewayunivers
 $config = array(
 
 	'cod_affiliate' => '1214449',
-	'credentials'   => base64_encode('alcaldiasucre01:Alcaldia2015-+'),
+	'credentials'   => base64_encode('alcaldiasucre01:Alcaldia2016-+'),
 	'pre_register'  => $url_pro . 'prereg?cod_afiliacion=@cod_affiliate&factura=@invoice_number&monto=@amount',
 	'verifier'      => $url_pro . 'querystatus?control=@control',
 	'redirect'		=> $url_pro . 'data?control=@control'
